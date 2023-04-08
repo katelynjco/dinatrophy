3c6cc46a1b6ecaddd4cce81ea126e18165d4dc2d
Katelyn <kjcollins7@gmail.com>
2023-04-08 17:24:21 -0400

    animation and score screen

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const yellowDino = document.getElementById("yellowDino");
 const purpleDino = document.getElementById("purpleDino");
 const greenDino = document.getElementById("greenDino");
 const blueDino = document.getElementById("blueDino");
+const start = document.getElementById("start");
+const continueBtn = document.getElementById("continue-btn");
+const death = document.getElementById("death-container");
 
 //canvas variables
 const canvas = document.getElementById("canvas");
@@ -14,23 +17,18 @@ const ctx = canvas.getContext("2d");
 let highScore = parseInt(localStorage.getItem("highScore")) || 0;
 let startingScore = 0;
 let continueAnimating = false;
-let score;
+let score = '';
 let elapsedTime = 0;
 let lastRockUpdate = Date.now();
 const rockImages = ["img/meteor1.png", "img/meteor2.png", "img/meteor3.png", "img/meteor4.png", "img/meteor5.png", "img/meteor6.png", "img/meteor7.png", "img/meteor8.png"];
 let dino = '';
 
 // block variables
+let dinoFacingLeft = false;
 let blockWidth = '';
 let blockHeight = '';
 let blockSpeed = 10;
-let block = {
-    x: 0,
-    y: canvas.height - blockHeight,
-    width: blockWidth,
-    height: blockHeight,
-    blockSpeed: blockSpeed
-}
+let block = {};
 
 // randomize variables
 function randomNumber(min, max) {
@@ -38,8 +36,8 @@ function randomNumber(min, max) {
   }
 
 // rock variables
-let rockWidth = "";
-let rockHeight = "";
+let rockWidth = '';
+let rockHeight = '';
 let totalRocks = 10;
 let fallingRocks = 0;
 let rocks = [];
@@ -62,11 +60,6 @@ function addRock() {
     }
 }
 
-// function resetGame() {
-//     // alert("YOU DIED!");
-//     window.location.reload();
-// }
-
 // move the rock to a random position near the top-of-canvas
 // assign the rock a random speed
 function resetRock(rock) {
@@ -81,12 +74,13 @@ function resetRock(rock) {
 document.onkeydown = function (event) {
     if (event.keyCode == 39) {
         block.x += block.blockSpeed;
-        // if (block.x >= canvas.width - block.width) {
-        //     continueAnimating = false;
-        //     alert("Completed with a score of " + score);
-        // }
+        dinoFacingLeft = false;
+        if (block.x >= (canvas.width - blockWidth)) {
+            block.x = canvas.width - blockWidth;
+        }
     } else if (event.keyCode == 37) {
         block.x -= block.blockSpeed;
+        dinoFacingLeft = true;
         if (block.x <= 0) {
             block.x = 0;
         }
@@ -96,11 +90,22 @@ document.onkeydown = function (event) {
 // function to save the high score to local storage
 function saveHighScore() {
     localStorage.setItem("highScore", highScore.toString());
-  }
+}
 
+// restart game
+function restartGame() {
+   location.reload(); 
+}
 
+// death screen
+function youDied() {
+    death.style.visibility = "visible";
+    continueAnimating = false;
+    continueBtn.addEventListener('click', restartGame);
+}
+
+// animate game and elements
 function animate() {
-
     // request another animation frame
     const now = Date.now();
     elapsedTime += now - lastRockUpdate;
@@ -121,16 +126,14 @@ function animate() {
 
         // test for rock-block collision and end game
         if (isColliding(rock, block)) {
-            alert("Completed with a score of " + score);
-            continueAnimating = false;
-            location.reload();
+            youDied();
         }
 
         // advance the rocks
         rock.y += rock.speed;
 
         // if the rock is below the canvas,
-        if (rock.y > canvas.height) {
+        if (rock.y > (canvas.height-5)) {
             score += 1;
             if (score > highScore) {
                 highScore = score;
@@ -152,40 +155,57 @@ function animate() {
 
     // redraw everything
     drawAll();
-
 }
 
+// collision detection
 function isColliding(a, b) {
     return !(
     b.x > a.x + a.width || b.x + b.width < a.x || b.y > a.y + a.height || b.y + b.height < a.y);
 }
 
+// generate game
 function drawAll() {
-
     // clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-     // load the image
-     ctx.fillStyle = "#A56000";
+    // load the image
+    ctx.fillStyle = "#A56000";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-     let backgroundImage = new Image();
-     backgroundImage.src = "img/background.png";
+    let backgroundImage = new Image();
+    backgroundImage.src = "img/background.png";
+
     // draw the background image
     ctx.drawImage(backgroundImage, -5, -5, (canvas.width + 10), (canvas.height + 10));
 
-    // draw the block
+    // draw the player
     ctx.fillStyle = "#B278E5";
     let playerImage = new Image();
-    if (dino === "bonusDinosaur") {
-        playerImage.src = "img/bonusDino.png";
-    } else if (dino === "yellowDinosaur") {
-        playerImage.src = "img/yellowDino.png";
-    } else if (dino === "purpleDinosaur") {
-        playerImage.src = "img/purpleDino.png";
-    } else if (dino === "greenDinosaur") {
-        playerImage.src = "img/greenDino.png";
-    } else if (dino === "blueDinosaur") {
-        playerImage.src = "img/blueDino.png";
+
+    // flip the dino if the player changes directions
+    if (dinoFacingLeft === true) {
+        if (dino === "bonusDinosaur") {
+            playerImage.src = "img/bonusDino.png";
+        } else if (dino === "yellowDinosaur") {
+            playerImage.src = "img/yellowDino.png";
+        } else if (dino === "purpleDinosaur") {
+            playerImage.src = "img/purpleDino.png";
+        } else if (dino === "greenDinosaur") {
+            playerImage.src = "img/greenDino.png";
+        } else if (dino === "blueDinosaur") {
+            playerImage.src = "img/blueDino.png";
+        }
+    } else {
+        if (dino === "bonusDinosaur") {
+            playerImage.src = "img/bonusDinoRight.png";
+        } else if (dino === "yellowDinosaur") {
+            playerImage.src = "img/yellowDinoRight.png";
+        } else if (dino === "purpleDinosaur") {
+            playerImage.src = "img/purpleDinoRight.png";
+        } else if (dino === "greenDinosaur") {
+            playerImage.src = "img/greenDinoRight.png";
+        } else if (dino === "blueDinosaur") {
+            playerImage.src = "img/blueDinoRight.png";
+        }
     }
     ctx.drawImage(playerImage, block.x, block.y, block.width, block.height);
 
@@ -198,16 +218,16 @@ function drawAll() {
     }
 
     // draw the score
-    ctx.font = "14px Architects Daughter";
+    ctx.font = "30px Architects Daughter";
     ctx.fillStyle = "#321300";
-    ctx.fillText("Score: " + score, 10, 15);
+    ctx.fillText(score, (canvas.width - (canvas.width/2)), 31);
 }
 
 // button to start the game
 function startGame() {
     if (dino === "bonusDinosaur" || dino === "yellowDinosaur"|| dino === "purpleDinosaur"|| dino === "greenDinosaur"|| dino === "blueDinosaur" ) {
         menu.style.visibility = "hidden";
-        score = startingScore
+        score = startingScore;
         block.x = 0;
         addRock();
         for (let i = 0; i < rocks.length; i++) {
@@ -287,7 +307,7 @@ bonusDino.addEventListener("click", function() {
   });
 
 function highScoreReward() {
-    if (highScore > 69) {
+    if (highScore > 200) {
         bonusDino.hidden = false;
     } else {
         bonusDino.hidden = true;
@@ -295,5 +315,4 @@ function highScoreReward() {
 }
 
 highScoreReward();
-const start = document.getElementById("start");
 start.addEventListener('click', startGame);
